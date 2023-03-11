ee0ae70610e1be3e91eaca9fe37f4c962ddbd9eb
olhaSabadina <oll04181527@gmail.com>
2023-03-12 01:07:45 +0200

    add storyboard

--- WeatherBrick/ViewController.swift
+++ WeatherBrick/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startLocationManager()
     }
 
-
+    func startLocationManager() {
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.pausesLocationUpdatesAutomatically = false
+            locationManager.startUpdatingHeading()
+        }
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            print(lastLocation.coordinate)
+        }
+    }
+}
